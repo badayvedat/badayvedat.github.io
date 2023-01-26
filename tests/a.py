6d9de895356a7cb27e510edab30d61f082ae6160
Vedat Baday <vedat.baday@udemy.com>
2023-01-26 17:14:48 +0300

    M

--- tests/a.py
+++ tests/a.py
@@ -2,7 +2,6 @@ import time
 
 
 for i in range(100):
-    time.sleep(1)
     print(f"a: {i}")
 
 "git show -s --format=%B | tail -2 | head -1"
--- tests/b.py
+++ tests/b.py
@@ -2,7 +2,6 @@ import time
 
 
 for i in range(200):
-    time.sleep(0.5)
     print(f"b: {i}")
 
 print("Not exists in master")
