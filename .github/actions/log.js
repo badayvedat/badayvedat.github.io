b2d2c388c3440df1902a8c6ab2e411bb8bf82b94
Vedat Baday <vedat.baday@udemy.com>
2023-01-27 18:50:04 +0300

    Move to function

--- .github/actions/log.js
+++ .github/actions/log.js
@@ -1,23 +1,25 @@
-
-require('dotenv').config();
-
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-let body = "<details><summary>Show Output</summary>```\n\nIntegration test details\n```</details>"
+async function run() {
+    let body = "<details><summary>Show Output</summary>```\n\nIntegration test details\n```</details>"
 
-console.log(process.env)
+    console.log(process.env)
 
-const octokit = github.getOctokit()
+    const octokit = new github.getOctokit()
 
-const response = octokit.rest.issues.createComment({
-    issue_number: context.issue.number,
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    body: body
-});
+    const response = octokit.rest.issues.createComment({
+        issue_number: context.issue.number,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        body: body
+    });
 
-const comment_id = response.data.id;
+    const comment_id = response.data.id;
 
-console.log(response);
-console.log(github.context);
+    console.log(response);
+    console.log(github.context);
+
+}
+
+run();
