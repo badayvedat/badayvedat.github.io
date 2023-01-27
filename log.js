6c0c2bb4472f0aaa3187cc23be87453acb76e93d
Vedat Baday <vedat.baday@udemy.com>
2023-01-27 17:34:09 +0300

    Add js

--- log.js
+++ log.js
@@ -1,10 +1,33 @@
+
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  // `who-to-greet` input defined in action metadata file
+  // const nameToGreet = core.getInput('who-to-greet');
+  // console.log(`Hello ${nameToGreet}!`);
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
+
 let body = "<details><summary>Show Output</summary>```\n\nIntegration test details\n```</details>"
-response = await github.rest.issues.createComment({
+
+
+
+
+response =  github.rest.issues.createComment({
     issue_number: context.issue.number,
     owner: context.repo.owner,
     repo: context.repo.repo,
     body: body
 });
+
 const comment_id = response.data.id;
-console.log(comment_id);
+
+console.log(response);
 console.log(context);
