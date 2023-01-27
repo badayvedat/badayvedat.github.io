8e04eef2bc564022227966f0daaf995a3665165c
Vedat Baday <vedat.baday@udemy.com>
2023-01-27 19:05:26 +0300

    Add try catch

--- .github/actions/log.js
+++ .github/actions/log.js
@@ -2,22 +2,26 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function run() {
-    let body = "<details><summary>Show Output</summary>```\n\Details\n```</details>"
-    console.log(process.env["GITHUB_TOKEN"].length)
-    const octokit = new github.GitHub(process.env["GITHUB_TOKEN"]);
+    try {
+        let body = "<details><summary>Show Output</summary>```\n\Details\n```</details>"
+        console.log(process.env["GITHUB_TOKEN"].length)
+        const octokit = github.getOctokit(process.env["GITHUB_TOKEN"]);
 
-    const response = octokit.rest.issues.createComment({
-        issue_number: context.issue.number,
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        body: body
-    });
+        const response = await octokit.rest.issues.createComment({
+            issue_number: context.issue.number,
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            body: body
+        });
 
-    const comment_id = response.data.id;
+        const comment_id = response.data.id;
 
-    console.log(response);
-    console.log(github.context);
-
+        console.log(response);
+        console.log(github.context);
+    } catch (error) {
+        core.error(error);
+        core.setFailed(error.message);
+    }
 }
 
 run();
