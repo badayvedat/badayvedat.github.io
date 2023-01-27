aa49c4439af90012740a64821bf90114cdb59d55
Vedat Baday <vedat.baday@udemy.com>
2023-01-27 17:51:10 +0300

    Test workflow

--- log.js
+++ log.js
@@ -2,25 +2,12 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  // const nameToGreet = core.getInput('who-to-greet');
-  // console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
-
 let body = "<details><summary>Show Output</summary>```\n\nIntegration test details\n```</details>"
 
+const token = core.getInput("GITHUB_TOKEN")
+const octokit = github.getOctokit(token)
 
-
-
-response =  github.rest.issues.createComment({
+const response = octokit.rest.issues.createComment({
     issue_number: context.issue.number,
     owner: context.repo.owner,
     repo: context.repo.repo,
@@ -30,4 +17,4 @@ response =  github.rest.issues.createComment({
 const comment_id = response.data.id;
 
 console.log(response);
-console.log(context);
+console.log(github.context);
