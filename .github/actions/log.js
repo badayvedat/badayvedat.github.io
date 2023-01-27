d809317afe1f85e4782e54b2f3b0eab62a1b5e6f
Vedat Baday <vedat.baday@udemy.com>
2023-01-27 19:41:54 +0300

    Move definiton to function

--- .github/actions/log.js
+++ .github/actions/log.js
@@ -1,24 +1,28 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+
+const createComment = async (body) => {
+    response = await octokit.rest.issues.createComment({
+        issue_number: context.issue.number,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        body: body
+    });
+    return response.data.id
+}
+
+
 async function run() {
     try {
         let body = "<details><summary>Show Output</summary>\n\n```\nDetails\n```\n</details>"
         
         const octokit = github.getOctokit(process.env["GITHUB_TOKEN"]);
         const { context = {} } = github;
-        
-        const response = await octokit.rest.issues.createComment({
-            issue_number: context.issue.number,
-            owner: context.repo.owner,
-            repo: context.repo.repo,
-            body: body
-        });
 
-        const comment_id = response.data.id;
+        const comment_id = createComment();
 
-        console.log(response);
-        console.log(github.context);
+        console.log(comment_id);
     } catch (error) {
         core.error(error);
         core.setFailed(error.message);
