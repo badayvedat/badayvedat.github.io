293014e878c1b79f0a496b85fa56841b0e57b7a3
Vedat Baday <vedat.baday@udemy.com>
2023-01-28 03:40:02 +0300

    Decrease a tests range

--- tests/a.py
+++ tests/a.py
@@ -3,7 +3,7 @@ import logging
 
 logging.basicConfig(filename='output.log', level=logging.INFO)
 
-for i in range(10000):
+for i in range(1000):
     time.sleep(0.1)
     logging.info(f"log a: {i}")
     print(f"a: {i}")
