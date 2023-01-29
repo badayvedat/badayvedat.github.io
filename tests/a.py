c400a0c5daba3e6a987554fee6cecf5f9af4b6bc
Vedat Baday <vedat.baday@udemy.com>
2023-01-29 16:16:09 +0300

    Increase range

--- tests/a.py
+++ tests/a.py
@@ -3,7 +3,6 @@ import logging
 
 logging.basicConfig(filename='output.log', level=logging.INFO)
 
-for i in range(100):
-    time.sleep(0.1)
+for i in range(100 * 1000):
     logging.info(f"log a: {i}")
     print(f"a: {i}")
